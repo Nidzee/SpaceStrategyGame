54eeaa2469682f06afc5e0824daea9c96af3b28b
Nidzee <romanbaboryga@gmail.com>
2021-02-17 00:35:25 +0200

    Game Menu and Map Generation
    Added basics of game menu and Tile map generator system

--- space_strategy/Assets/Scripts/Tile Managment/MapGenerator.cs
+++ space_strategy/Assets/Scripts/Tile Managment/MapGenerator.cs
@@ -18,80 +18,121 @@ public class MapGenerator : MonoBehaviour
     private int MapSizeColumn = 30;
     private int MapSizeRow = 30;
 
+    int[,] myArr;
+
     private void Start()
     {
-        GenerateMap();
+        GanarteArray();
+        CreateMapFromArray();
     }
 
-    private void GenerateMap()
+    private void GanarteArray()
     {
-        for (int column = 0; column <=MapSizeColumn; column++)
+        myArr = new int[MapSizeColumn, MapSizeRow];
+
+        // 0 - Map Edges
+        // 1 - Free tile
+        // 2 - Closed Tiles
+        // 3 - RS1
+        // 4 - RS2
+        // 5 - RS3
+
+        for (int column = 0; column < MapSizeColumn; column++)
         {
-            for (int row = 0; row <= MapSizeRow; row++)
+            for (int row = 0; row < MapSizeRow; row++)
             {
-                GameObject hexGO = Instantiate(hexPrefab_FreeTile, Vector3.zero, Quaternion.identity, this.transform);
-                temp = hexGO.GetComponent<Hex>();
-                temp.Initialize_with_arr_pos(column, row);
+                myArr[column, row] = 1;
+
+                if((column == 0 || column == MapSizeColumn) || (row == 0 || row == MapSizeRow)
+                || (column == 1 || column == MapSizeColumn-1) || (row == 1 || row == MapSizeRow-1))
+                    myArr[column, row] = 0;
+
+
+                if(column == 5 && row == 4)
+                {
+                    myArr[column, row] = 2;
+                }
+                if(column == 5 && row == 5)
+                {
+                    myArr[column, row] = 2;
+                }
+                if(column == 6 && row == 4)
+                {
+                    myArr[column, row] = 2;
+                }
+                if(column == 6 && row == 5)
+                {
+                    myArr[column, row] = 2;
+                }
+            }
+        }
+    }
+
+    private void CreateMapFromArray()
+    {
+        for (int column = 0; column < MapSizeColumn; column++)
+        {
+            for (int row = 0; row < MapSizeRow; row++)
+            {
+                GameObject hexGO = null;
+
+                switch (myArr[column, row])
+                {
+                    case 0:
+                    hexGO = Instantiate(hexPrefab_MapEdge, Vector3.zero, Quaternion.identity, this.transform);
+                    temp = hexGO.GetComponent<Hex>();
+                    temp.Initialize_with_arr_pos(column, row);
+                    temp.tile_Type = Tile_Type.MapEdge;
+                    break;
+
+                    case 1:
+                    hexGO = Instantiate(hexPrefab_FreeTile, Vector3.zero, Quaternion.identity, this.transform);
+                    temp = hexGO.GetComponent<Hex>();
+                    temp.Initialize_with_arr_pos(column, row);
+                    temp.tile_Type = Tile_Type.FreeTile;
+                    break;
+
+                    case 2:
+                    hexGO = Instantiate(hexPrefab_ClosedTile, Vector3.zero, Quaternion.identity, this.transform);
+                    temp = hexGO.GetComponent<Hex>();
+                    temp.Initialize_with_arr_pos(column, row);
+                    temp.tile_Type = Tile_Type.ClosedTile;
+                    break;
+
+                    case 3: 
+                    hexGO = Instantiate(hexPrefab_RS1_crystal, Vector3.zero, Quaternion.identity, this.transform);
+                    temp = hexGO.GetComponent<Hex>();
+                    temp.Initialize_with_arr_pos(column, row);
+                    temp.tile_Type = Tile_Type.RS1_crystal;
+                    break;
+
+                    case 4: 
+                    hexGO = Instantiate(hexPrefab_RS2_iron, Vector3.zero, Quaternion.identity, this.transform);
+                    temp = hexGO.GetComponent<Hex>();
+                    temp.Initialize_with_arr_pos(column, row);
+                    temp.tile_Type = Tile_Type.RS2_iron;
+                    break;
+
+                    case 5:
+                    hexGO = Instantiate(hexPrefab_RS3_gel, Vector3.zero, Quaternion.identity, this.transform);
+                    temp = hexGO.GetComponent<Hex>();
+                    temp.Initialize_with_arr_pos(column, row);
+                    temp.tile_Type = Tile_Type.RS3_gel;
+                    break;
+
+                    case 6: 
+                    hexGO = Instantiate(hexPrefab_EnemyTile, Vector3.zero, Quaternion.identity, this.transform);
+                    temp = hexGO.GetComponent<Hex>();
+                    temp.Initialize_with_arr_pos(column, row);
+                    temp.tile_Type = Tile_Type.EnemyTile;
+                    break;
+                }
+
                 hexGO.transform.position = temp.Position();
                 //hexGO.name = string.Format(column + "." + row);
                 hexGO.name = string.Format(temp.Q + "." + temp.R + "." + temp.S);
 
                 SpriteRenderer sr = hexGO.GetComponentInChildren<SpriteRenderer>();
-                
-                if((column == 0 || column == MapSizeColumn) || (row == 0 || row == MapSizeRow)
-                || (column == 1 || column == MapSizeColumn-1) || (row == 1 || row == MapSizeRow-1))
-                    hexGO.GetComponent<Hex>().tile_Type = Tile_Type.MapEdge;
-
-
-                if(column == 15 && row == 15)
-                {
-                    mapCenter = temp.Position();
-                }
-
-
-
-                if(column == 5 && row == 4)
-                {
-                    hexGO.GetComponent<Hex>().tile_Type = Tile_Type.ClosedTile;
-                }
-                if(column == 5 && row == 5)
-                {
-                    hexGO.GetComponent<Hex>().tile_Type = Tile_Type.ClosedTile;
-                }
-                if(column == 6 && row == 4)
-                {
-                    hexGO.GetComponent<Hex>().tile_Type = Tile_Type.ClosedTile;
-                }
-                if(column == 6 && row == 5)
-                {
-                    hexGO.GetComponent<Hex>().tile_Type = Tile_Type.ClosedTile;
-                }
-
-                
-                switch(hexGO.GetComponent<Hex>().tile_Type)
-                {
-                    case Tile_Type.MapEdge: sr.color = Color.gray; 
-                    break;
-
-                    case Tile_Type.FreeTile: sr.color = Color.green;
-                    break;
-
-                    case Tile_Type.ClosedTile: sr.color = Color.red;
-                    break;
-
-                    case Tile_Type.RS1_crystal: sr.color = Color.yellow;
-                    break;
-
-                    case Tile_Type.RS2_iron: sr.color = Color.yellow;
-                    break;
-
-                    case Tile_Type.RS3_gel: sr.color = Color.yellow;
-                    break;
-
-                    case Tile_Type.EnemyTile: sr.color = Color.magenta;
-                    break;
-                }
-                
             }
         }
     }
